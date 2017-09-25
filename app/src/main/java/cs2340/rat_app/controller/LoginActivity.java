0183bed6bdee890ffe665a91083a8ddcd0eac87b
Colton Mack <colton.mack@gmail.com>
2017-09-25 13:04:49 -0400

    Updated LoginActivity and RegisterActivity

--- app/src/main/java/cs2340/rat_app/controller/LoginActivity.java
+++ app/src/main/java/cs2340/rat_app/controller/LoginActivity.java
@@ -5,12 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
 import cs2340.rat_app.model.Account;
-
-
 import cs2340.rat_app.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +33,12 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
     }
+
     public void login() {
-        if (Account.getUserList().containsKey(loginUser.getText())) {
-            String storedPassword = usersMap.get(loginUser.getText());
-            if (storedPassword.equals(loginPassword.getPassword())) {
-                // valid login
+        if (Account.getUserList().containsKey(username.getText().toString())) {
+            String storedPassword = password.getText().toString();
+            if (storedPassword.equals(Account.getUserList().get(username))) {
+                System.out.println("Login successfull!");
             } else {
                 System.out.println("Invalid password");
             }
--- app/src/main/java/cs2340/rat_app/controller/RegisterActivity.java
+++ app/src/main/java/cs2340/rat_app/controller/RegisterActivity.java
@@ -14,6 +14,7 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText lastname;
     private EditText username;
     private EditText password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
