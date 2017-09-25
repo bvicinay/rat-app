e84ef5fd7f6b33632df474e5c26aa45dbb21c21e
tbelford <thomasbelford32@gmail.com>
2017-09-24 22:21:04 -0400

    Connects register button to register screen, but register screen needs to be worked on.

--- app/src/main/java/cs2340/rat_app/controller/LoginActivity.java
+++ app/src/main/java/cs2340/rat_app/controller/LoginActivity.java
@@ -1,7 +1,10 @@
 package cs2340.rat_app.controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import cs2340.rat_app.R;
 
@@ -11,5 +14,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Button goToRegister = (Button) findViewById(R.id.button2);
+        /*goToRegister.setOnClickListener((view) -> {
+            Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
+        });*/
+
+    }
+    public void goToRegister(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
