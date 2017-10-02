5c81b172f6a011a86fe24355ed5be64a189e4216
tbelford <thomasbelford32@gmail.com>
2017-10-02 08:39:30 -0400

    Javadoc'd Account.java

--- app/src/main/java/cs2340/rat_app/model/Account.java
+++ app/src/main/java/cs2340/rat_app/model/Account.java
@@ -2,6 +2,10 @@ package cs2340.rat_app.model;
 
 import java.io.Serializable;
 
+/**
+ * Account is the class that represents an account in the system. Each account has a username,
+ * password, account type, firstname, lastname, and email
+ */
 public class Account implements Serializable {
 
     //Account Type
@@ -16,8 +20,15 @@ public class Account implements Serializable {
     private String lastName;
     private String email;
 
-
-
+    /**
+     * The constructor for the account. Sets the instance data.
+     * @param accountType AccountType enum to declare the account type (user or admin).
+     * @param username String that will be set as the username.
+     * @param password String that will be set as the password.
+     * @param firstName String that will be set as the first name for the account.
+     * @param lastName String that will be set as the last name for the account.
+     * @param email String that will be set as the email of the account.
+     */
     public Account(AccountType accountType, String username, String password, String firstName,
                    String lastName, String email) {
         this.AccountType = accountType;
@@ -28,9 +39,18 @@ public class Account implements Serializable {
         this.email = email;
     }
 
+    /**
+     * Getter method for the username.
+     * @return String representing the userName.
+     */
     public String getUsername() {
         return userName;
     }
+
+    /**
+     * Getter method for the password.
+     * @return String representing the password.
+     */
     public String getPassword() {
         return password;
     }
