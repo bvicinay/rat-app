784337ac7fc57fe2216e876b4dd89c6479701717
Borja Vicinay <bvicinay@gmail.com>
2017-10-02 16:31:49 -0400

    Added JavaDocs to AccountLIst

--- app/src/main/java/cs2340/rat_app/model/AccountList.java
+++ app/src/main/java/cs2340/rat_app/model/AccountList.java
@@ -15,32 +15,55 @@ public class AccountList {
         }
     }
 
-    public AccountList() {};
+    public AccountList() {}
 
     /**
-     *
-     * @param act
+     * Adds an account to the account list
+     * @param act account object to be added
      */
     public static void add(Account act) {
         AccountList.accounts.put(act.getUsername(), act);
         size++;
     }
 
+    /**
+     * Removes an account from the account list
+     * @param act account object to be removed
+     * @return removed account
+     */
     public static Account remove(Account act) {
         size--;
         return accounts.remove(act.getUsername());
     }
+
+    /**
+     * Removes an account from the account list
+     * @param username username string of account to be removed
+     * @return removed account
+     */
     public static Account remove(String username) {
         size--;
         return accounts.remove(username);
     }
 
+    /**
+     * Accounts getter
+     * @return hashmap with usernames and accounts
+     */
     public static HashMap<String, Account> getAccounts() {return accounts;}
 
+    /**
+     * Size getter
+     * @return int number of accounts
+     */
     public static int size() {
         return size;
     }
 
+    /**
+     * Empty list getter
+     * @return boolean representing whether list is empty
+     */
     public static boolean isEmpty() {return size == 0;}
 
 }
