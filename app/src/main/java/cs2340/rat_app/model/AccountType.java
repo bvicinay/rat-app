360bfafed19a5439f6b327f336eaf6fd364e3301
tbelford <thomasbelford32@gmail.com>
2017-10-02 08:46:05 -0400

    Javadoc'd AccountType.java

--- app/src/main/java/cs2340/rat_app/model/AccountType.java
+++ app/src/main/java/cs2340/rat_app/model/AccountType.java
@@ -1,11 +1,19 @@
 package cs2340.rat_app.model;
 
+/**
+ * AccountType is an enum representing the types of accounts that are possible.
+ * Right now, just User and Admin.
+ */
 public enum AccountType {
     USER("USER"),
     ADMIN("ADMINISTRATOR");
 
     private String name;
 
+    /**
+     * Constructor for the account type.
+     * @param name String that will be used to determine the type of the account.
+     */
     AccountType(String name) {
         this.name = name;
     }
