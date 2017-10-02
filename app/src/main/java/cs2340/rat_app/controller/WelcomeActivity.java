45bf4b9891153750892430849f8a42d346c383c3
tbelford <thomasbelford32@gmail.com>
2017-10-02 08:32:53 -0400

    Javadoc'd welcomeActivity

--- app/src/main/java/cs2340/rat_app/controller/WelcomeActivity.java
+++ app/src/main/java/cs2340/rat_app/controller/WelcomeActivity.java
@@ -7,6 +7,10 @@ import android.view.View;
 
 import cs2340.rat_app.R;
 
+/**
+ * WelcomeActivity is the controller for the activity_welcome screen (screen the app
+ * goes to after login is completed and successful).
+ */
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
@@ -15,6 +19,11 @@ public class WelcomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
     }
 
+    /**
+     * returnToLogin is the method that is called when the user presses the back button.
+     * Returns the user to the original login screen and logs them out.
+     * @param view the view that the calls the method.
+     */
     public void returnToLogin(View view) {
         Intent intent = new Intent(this, LoginActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
