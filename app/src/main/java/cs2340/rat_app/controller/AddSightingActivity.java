4bea049d175cf82a5510e2ae12dac55e6e925b56
Colton Mack <colton.mack@gmail.com>
2017-10-24 19:40:03 -0400

    added report_sighting_activity

--- app/src/main/java/cs2340/rat_app/controller/AddSightingActivity.java
+++ app/src/main/java/cs2340/rat_app/controller/AddSightingActivity.java
@@ -1,6 +1,8 @@
 package cs2340.rat_app.controller;
 
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by colton on 10/23/17.
@@ -8,5 +10,14 @@ import android.support.v7.app.AppCompatActivity;
 
 public class AddSightingActivity extends AppCompatActivity {
 
+    private TextView key;
+    private TextView date;
+    private EditText locType;
+    private EditText zipCode;
+    private EditText address;
+    private EditText city;
+    private EditText borough;
+    private EditText latitude;
+    private EditText longitude;
     
 }
