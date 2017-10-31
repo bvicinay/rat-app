4f4f2d3cb7b40da291c74c989311938dbe6ce744
trobinson80 <tgrobinson42@gmail.com>
2017-10-30 20:45:27 -0400

    map now adds markers for 1st 20 rat sighting locations

--- app/src/main/java/cs2340/rat_app/controller/MapsActivity.java
+++ app/src/main/java/cs2340/rat_app/controller/MapsActivity.java
@@ -10,6 +10,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import cs2340.rat_app.R;
+import cs2340.rat_app.model.RatSighting;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -39,9 +40,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        int count = 0;
+
+        RatSighting rat = RatSighting.ratSightings.get(0);
+
+        for (int i = 0; i < RatSighting.ratSightings.size(); i++) {
+
+            rat = RatSighting.ratSightings.get(i);
+
+            mMap.addMarker(new MarkerOptions().position(new LatLng(rat.getLocation().getLatitude(), rat.getLocation().getLongitude())).title("Sighting " + i));
+
+            count++;
+
+            if (count == 20) {
+                break;
+            }
+        }
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(rat.getLocation().getLatitude(), rat.getLocation().getLongitude())));
+
+
+
     }
 }
