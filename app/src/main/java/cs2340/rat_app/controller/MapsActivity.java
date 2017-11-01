c57be59bd95171f9fae5aa64f477826075d3907d
Borja Vicinay <bvicinay@gmail.com>
2017-11-01 13:24:29 -0400

    Add back button on MapsActivity

--- app/src/main/java/cs2340/rat_app/controller/MapsActivity.java
+++ app/src/main/java/cs2340/rat_app/controller/MapsActivity.java
@@ -1,9 +1,12 @@
 package cs2340.rat_app.controller;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -27,6 +30,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     protected GoogleMap mMap;
 
+    private Button backButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +40,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // When register button is pressed
+        backButton = (Button) findViewById(R.id.map_back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getOuter(), RatSightingListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 
 
@@ -96,4 +113,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
     }
+
+    private MapsActivity getOuter() { return this; }
 }
