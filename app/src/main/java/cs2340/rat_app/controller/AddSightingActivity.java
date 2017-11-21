9103976092783737625bac5063af549073bcfc84
Colton Mack <colton.mack@gmail.com>
2017-11-21 03:05:48 -0500

    more cleaning

--- app/src/main/java/cs2340/rat_app/controller/AddSightingActivity.java
+++ app/src/main/java/cs2340/rat_app/controller/AddSightingActivity.java
@@ -120,14 +120,10 @@ public class AddSightingActivity extends AppCompatActivity {
         String latitude = lat.toString();
         Editable lon = longitudeField.getText();
         String longitude = lon.toString();
-        return validateHelper(locType, zipCode, address, city, borough,
-                latitude, longitude);
-    }
 
-    private boolean validateHelper(String locType, String zipCode, String address, String city,
-                                   String borough, String latitude, String longitude) {
         boolean valid = true;
         View focusView = null;
+
         if (!validateLocType(locType)) {
             locTypeField.setError("This field is required");
             focusView = locTypeField;
