b67c0910da8027a064e71189e4721451095d2a89
tbelford <thomasbelford32@gmail.com>
2017-11-24 17:43:17 -0500

    added location services to map, should open to your location when map is opened.

--- app/src/main/java/cs2340/rat_app/controller/MapsActivity.java
+++ app/src/main/java/cs2340/rat_app/controller/MapsActivity.java
@@ -1,17 +1,25 @@
 package cs2340.rat_app.controller;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.Button;
 import android.location.Location;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import java.util.Calendar;
 import java.util.List;
@@ -28,6 +36,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private Calendar startDate;
     private Calendar finishDate;
+    private FusedLocationProviderClient fusedLoc;
+    private boolean mLocationPermissionGranted;
+    private Location mlastKnownLoc;
+    private static final int DEFAULT_ZOOM = 15;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    //default loc is New York City
+    private final LatLng mDefaultLoc = new LatLng(40.7128, 74.0060);
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +52,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Intent in = getIntent();
         startDate = (Calendar) in.getSerializableExtra("startDate");
         finishDate = (Calendar) in.getSerializableExtra("finishDate");
+        //create fused location provider client
+        fusedLoc = LocationServices.getFusedLocationProviderClient(this);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -66,18 +84,53 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        GoogleMap mMap = googleMap;
+        mMap = googleMap;
         try {
             List<RatSighting> sightings = RatList.getInstance();
             List<RatSighting> dateRangeRats;
 
             dateRangeRats = RatSighting.validateDataForMapAndGraph(sightings, startDate,
                     finishDate);
-
+            getLocationPermission();
             updateMapView(mMap, dateRangeRats);
+            updateLocationUI();
+            getDeviceLocation();
 
         } catch(Exception e) {
             Log.d("Exception", "no data in the range", e);
+        }
+    }
+    /**
+     * Gets the current location of the device, and positions the map's camera.
+     */
+    private void getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (mLocationPermissionGranted) {
+                Task<Location> locationResult = fusedLoc.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful()) {
+                            // Set the map's camera position to the current location of the device.
+                            mlastKnownLoc = task.getResult();
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(mlastKnownLoc.getLatitude(),
+                                            mlastKnownLoc.getLongitude()), DEFAULT_ZOOM));
+                        } else {
+
+                            mMap.moveCamera(CameraUpdateFactory
+                                    .newLatLngZoom(mDefaultLoc, DEFAULT_ZOOM));
+                            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
         }
     }
 
@@ -85,9 +138,71 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = RatSighting.filterMap(mMap, dateRangeRats);
         RatSighting rat = dateRangeRats.get(0);
         Location loc = rat.getLocation();
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(loc.getLatitude(),
-                loc.getLongitude())));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(loc.getLatitude(),
+        //        loc.getLongitude())));
     }
+
+    /**
+     * Prompts the user for permission to use the device location.
+     */
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+    /**
+     * Handles the result of the request for location permissions.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
+        updateLocationUI();
+    }
+
+    /**
+     * Updates the map's UI settings based on whether the user has granted location permission.
+     */
+    private void updateLocationUI() {
+        if (mMap == null) {
+            return;
+        }
+        try {
+            if (mLocationPermissionGranted) {
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            } else {
+                mMap.setMyLocationEnabled(false);
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                mlastKnownLoc = null;
+                getLocationPermission();
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
 
     private MapsActivity getOuter() { return this; }
 
